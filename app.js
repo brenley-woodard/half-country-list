bba7207fc21a9efcb576c8f76189696eda0f125a
brenley-woodard <brenleyew@gmail.com>
2022-11-09 11:42:12 -0800

    all countries (or first 100) display on page load

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* Imports */
 // Slice A: import getCountries from fetch-utils.js
+import { getCountries } from './fetch-utils.js';
 // Slice B: import getContinents from fetch-utils.js
 
 import { renderContinentOption, renderCountry } from './render-utils.js';
@@ -24,10 +25,13 @@ window.addEventListener('load', async () => {
 
 async function findCountries(continent) {
     // Slice A: call the asynchronous fetch function to get the countries
+    const response = await getCountries();
     // Slice C: add continent argument to getCountries function call
     // console log the response object to see all of the nested information returned
     // Slice A: set the countries state to the response.data
+    countries = response.data;
     // Slice A: call displayCountries function;
+    displayCountries();
 }
 
 searchForm.addEventListener('submit', (e) => {
@@ -39,9 +43,12 @@ searchForm.addEventListener('submit', (e) => {
 /* Display Functions */
 function displayCountries() {
     //Slice A: reset the countries List
+    countryList.innerHTML = '';
 
     for (const country of countries) {
         // Slice A: Call imported render countries function and append to list
+        const countryEl = renderCountry(country);
+        countryList.append(countryEl);
     }
 }
 
